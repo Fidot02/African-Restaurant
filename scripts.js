d8915cfe51380af4cff2bbf6706d7ca6ef357ae6
victororiahi <oriahiv99@gmail.com>
2025-07-08 16:11:44 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+    speed: 1000
+});
