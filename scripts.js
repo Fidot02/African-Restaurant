4de85cfd2124eafbfcdca8dd8ec7bb6884504570
victororiahi <oriahiv99@gmail.com>
2025-07-09 13:48:47 +0100

    Finalized and  adde ffhe reservation, testimonial and contact us section

--- scripts.js
+++ scripts.js
@@ -10,3 +10,19 @@ const swiper = new Swiper('.swiper', {
     },
     speed: 1000
 });
+
+
+//Scroll to top
+const scrollBtn = document.getElementById('scrollTopBtn');
+window.addEventListener('scroll', ()=>{
+    if(window.scrollY > window.innerHeight * 0.8){
+        scrollBtn.classList.add('show');
+    }
+    else{
+        scrollBtn.classList.remove('show');
+    }
+});
+
+scrollBtn.addEventListener('click', () => {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+});
